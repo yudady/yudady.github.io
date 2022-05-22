8497cb75d2bc326810d6538671dde121c8da4800
tommy <yudady@gmail.com>
2022-05-22 14:15:45 +0800

    fix : compile  error

--- app/src/main/java/practise/workspace/app/App.java
+++ app/src/main/java/practise/workspace/app/App.java
@@ -3,19 +3,8 @@
  */
 package practise.workspace.app;
 
-import practise.workspace.list.LinkedList;
-
-import static practise.workspace.utilities.StringUtils.join;
-import static practise.workspace.utilities.StringUtils.split;
-import static practise.workspace.app.MessageUtils.getMessage;
-
-import org.apache.commons.text.WordUtils;
-
 public class App {
     public static void main(String[] args) {
-        LinkedList tokens;
-        tokens = split(getMessage());
-        String result = join(tokens);
-        System.out.println(WordUtils.capitalize(result));
+        System.out.println(System.nanoTime());
     }
 }

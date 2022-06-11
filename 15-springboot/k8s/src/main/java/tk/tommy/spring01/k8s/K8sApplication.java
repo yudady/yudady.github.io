2260dacb13585eb1ebab637208feabbf4c241c4a
tommy <yudady@gmail.com>
2022-06-12 00:28:13 +0800

    init k8s spring bppt

---
+++ 15-springboot/k8s/src/main/java/tk/tommy/spring01/k8s/K8sApplication.java
@@ -0,0 +1,20 @@
+package tk.tommy.spring01.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class K8sApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sApplication.class, args);
+    }
+
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
---
+++ 15-springboot/k8s/src/test/java/tk/tommy/spring01/k8s/K8sApplicationTests.java
@@ -0,0 +1,13 @@
+package tk.tommy.spring01.k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

1133f66230c8449b8a4ed4a12786e00c2b01df4d
tommy <yudady@gmail.com>
2022-11-10 01:56:14 +0800

    add http fluentd

---
+++ monitoring/http-fluent/fluentbit/README.md
@@ -0,0 +1,8 @@
+https://ithelp.ithome.com.tw/articles/10279925
+
+
+docker-compose logs -f fluentd 
+
+
+
+use docker-from-docker-compose
---
+++ monitoring/http-fluent/fluentbit/input/app.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#!/bin/sh
+while true
+do
+	echo "Sending logs to FluentD"
+  curl -X POST -d 'json={"foo":"bar"}' http://fluentd:9880/http-myapp.log
+	sleep 3
+done
